51a05aabff18dfc40f53801e0318f7bcecf27c62
Mobius <energy908@gmail.com>
2018-11-25 15:58:21 +0800

    [Routing]-直接導向view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel @if(isset($var)) {{ $var }} @endif
                 </div>
 
                 <div class="links">
